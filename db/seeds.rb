dafd26ce58646234096d1e7b89e676803a55fd19
Johnathan <jocuog@gmail.com>
2022-05-16 11:17:26 -0600

    commit tree 8cd48ac4114bdde8419e0e042294ee0acbe7ffa0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts '🧀 Seeding cheeses...'
 
-Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
+Cheese.create!(name: 'cheddar', price: 3, is_best_seller: true)
 Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
 Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
 
